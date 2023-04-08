9f4337b05dab1c663923c6f6d94059ca38cbc518
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2023-04-08 17:09:14 +0900

    c++ problem 5 (NOT SOLVE)
    오타 제거하는 문제
    다시 풀어봐야함

--- resolve_c++/resolve_c++/코드.cpp
+++ resolve_c++/resolve_c++/코드.cpp
@@ -3,42 +3,30 @@
 
 using namespace std;
 
-struct answer {
-	int receive_num[10];
-	int aver;
-	int frequently_num;
-	int fre_n[10];
-	int fre_thenumber[10];
-};
+struct typing_mistake {
+	int size;
+	int tm_pos[10];
+	char tm_str[10][10];
+}tm;
 
-int main() {
-	answer ans;
-	ans.aver = 0;
+int main()
+{
 	int i, j;
-	for (i = 0; i < 10; i++)
-		ans.fre_thenumber[i] = 0;
 
-	for (i = 0; i < 10; i++) {
-		cin >> ans.receive_num[i];
-		if ((ans.receive_num[i] < 10 || ans.receive_num[i] > 1000) && ans.receive_num[i] % 10 != 0)
-			exit(0);
-		ans.aver += ans.receive_num[i];
-	}
-	ans.aver /= 10;
+	cin >> tm.size;
 
-	for (i = 0; i < 10; i++) {
-		ans.fre_n[i] = ans.receive_num[i];
-		for (j = 0; j < 10; j++)
-			if (ans.fre_n[i] == ans.receive_num[j])
-				ans.fre_thenumber[i]++;
-		if (i != 0) {
-			if (ans.fre_thenumber[i] > ans.fre_thenumber[i - 1])
-				ans.frequently_num = ans.receive_num[i];
-		}
-		else 
-			ans.frequently_num = ans.receive_num[0];
+	for (i = 0; i < tm.size; i++) {				
+		cin >> tm.tm_pos[i];
+		cin >> tm.tm_str[i];
+		for (j = 0; j < strlen(tm.tm_str[i]) - 1; j++)
+			if (isupper(tm.tm_str[i][j]) == 0)
+				exit(0);
 	}
-	
-	cout << ans.aver << endl; 
-	cout << ans.frequently_num << endl;
+
+	for (i = 0; i < tm.size; i++) {
+		for (j = tm.tm_pos[i] - 1; j < strlen(tm.tm_str[i]); j++)
+			tm.tm_str[i][j] = tm.tm_str[i][j + 1];
+		cout << tm.tm_str[i] << endl;
+	}
+	return 0;
 }
