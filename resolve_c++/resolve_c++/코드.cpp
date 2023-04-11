e361467215774c7bc1b016f073ffc783c2617c6b
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2023-04-11 18:13:36 +0900

    c++ problem 7
    10개의 수를 입력받아
    각각의 수를 42로 나눈 나머지의
    값이 다른 개수를 출력

--- resolve_c++/resolve_c++/코드.cpp
+++ resolve_c++/resolve_c++/코드.cpp
@@ -3,30 +3,27 @@
 
 using namespace std;
 
-struct typing_mistake {
-	int size;
-	int tm_pos[10];
-	char tm_str[10][10];
-}tm;
+struct THE_REST {
+	int receive_nums[10] = {0, };
+	int count_what_another_num = 1;
+}tr;
 
 int main()
 {
 	int i, j;
+	
+	for (i = 0; i < 10; i++)
+		cin >> tr.receive_nums[i];
 
-	cin >> tm.size;
-
-	for (i = 0; i < tm.size; i++) {				
-		cin >> tm.tm_pos[i];
-		cin >> tm.tm_str[i];
-		for (j = 0; j < strlen(tm.tm_str[i]) - 1; j++)
-			if (isupper(tm.tm_str[i][j]) == 0)
-				exit(0);
+	for (i = 1; i < 10; i++) {
+		for (j = i - 1; j >= 0; j--) {
+			if (tr.receive_nums[i] % 42 == tr.receive_nums[j] % 42) {
+				tr.count_what_another_num--;
+				break;
+			}
+		}
+		tr.count_what_another_num++;
 	}
 
-	for (i = 0; i < tm.size; i++) {
-		for (j = tm.tm_pos[i] - 1; j < strlen(tm.tm_str[i]); j++)
-			tm.tm_str[i][j] = tm.tm_str[i][j + 1];
-		cout << tm.tm_str[i] << endl;
-	}
-	return 0;
+	cout << tr.count_what_another_num << endl;
 }
