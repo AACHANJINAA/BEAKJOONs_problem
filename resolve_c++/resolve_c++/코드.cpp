f28343473bb5dfd821f44c2519911b958e909fee
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2023-04-12 17:24:10 +0900

    c++ problem 9
    입력 받을 정수의 총 개수,
    그 개수만큼의 정수들,
    그 정수들 중 찾을 정수를 입력받은 뒤
    찾을 정수가 몇개 있는지 출력

--- resolve_c++/resolve_c++/코드.cpp
+++ resolve_c++/resolve_c++/코드.cpp
@@ -3,34 +3,27 @@
 
 using namespace std;
 
-struct SCALE {
-	int sequence[1001] = {0, };
-	int the_biggest = 0;
-	int the_smallest = 0;
-	int ADD_price = 0;
-}scale;
+struct ESSENCE {
+	int total_num = 0;
+	int received_nums[100] = { 0, };
+	int find_num = 0;
+	int find_stack = 0;
+}ES;
 
 int main()
 {
-	int i, j, arr_num = 1;
+	int i;
 
-	for (i = 1; i < 46; i++) {
-		for (j = i; j > 0; j--){
-			scale.sequence[arr_num] = i;
-			arr_num++;
-			if (arr_num > 1000)
-				break;
-		}
-		if (arr_num > 1000)
-			break;
+	cin >> ES.total_num;
+	for (i = 0; i < ES.total_num; i++)
+		cin >> ES.received_nums[i];
+	cin >> ES.find_num;
+
+	for (i = 0; i < ES.total_num; i++) {
+		if (ES.received_nums[i] == ES.find_num)
+			ES.find_stack++;
 	}
 
-	cin >> scale.the_smallest;
-	cin >> scale.the_biggest;
+	cout << ES.find_stack;
 
-	for (i = scale.the_smallest; i <= scale.the_biggest; i++) {
-		scale.ADD_price += scale.sequence[i];
-	}
-
-	cout << scale.ADD_price;
 }
