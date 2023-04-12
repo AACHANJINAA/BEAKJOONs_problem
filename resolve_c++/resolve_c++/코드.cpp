eb506dbfc4c22e32d71b1f61e90a1f5b5ef881dd
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2023-04-12 16:47:57 +0900

    c++ problem 8
    1은 1개 2는 2개 이런 식으로 1000개의 수가 있는 수열을 만들고
    범위를 입력받아 그 범위 안에 있는 값들의 합을 구한다.

--- resolve_c++/resolve_c++/코드.cpp
+++ resolve_c++/resolve_c++/코드.cpp
@@ -3,27 +3,34 @@
 
 using namespace std;
 
-struct THE_REST {
-	int receive_nums[10] = {0, };
-	int count_what_another_num = 1;
-}tr;
+struct SCALE {
+	int sequence[1001] = {0, };
+	int the_biggest = 0;
+	int the_smallest = 0;
+	int ADD_price = 0;
+}scale;
 
 int main()
 {
-	int i, j;
-	
-	for (i = 0; i < 10; i++)
-		cin >> tr.receive_nums[i];
+	int i, j, arr_num = 1;
 
-	for (i = 1; i < 10; i++) {
-		for (j = i - 1; j >= 0; j--) {
-			if (tr.receive_nums[i] % 42 == tr.receive_nums[j] % 42) {
-				tr.count_what_another_num--;
+	for (i = 1; i < 46; i++) {
+		for (j = i; j > 0; j--){
+			scale.sequence[arr_num] = i;
+			arr_num++;
+			if (arr_num > 1000)
 				break;
-			}
 		}
-		tr.count_what_another_num++;
+		if (arr_num > 1000)
+			break;
 	}
 
-	cout << tr.count_what_another_num << endl;
+	cin >> scale.the_smallest;
+	cin >> scale.the_biggest;
+
+	for (i = scale.the_smallest; i <= scale.the_biggest; i++) {
+		scale.ADD_price += scale.sequence[i];
+	}
+
+	cout << scale.ADD_price;
 }
