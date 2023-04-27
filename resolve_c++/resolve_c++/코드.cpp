87960ab99df4fe3c461be13deef72afe0255293d
AACHANJINAA <110803149+AACHANJINAA@users.noreply.github.com>
2023-04-27 14:51:34 +0900

    c++ problem 10
    총 입력 받을 정수의 개수를 입력받고 그 개수만큼 정수들을 입력받아
    그 중 최솟값, 최댓값을 출력한다.

--- resolve_c++/resolve_c++/코드.cpp
+++ resolve_c++/resolve_c++/코드.cpp
@@ -3,27 +3,39 @@
 
 using namespace std;
 
-struct ESSENCE {
-	int total_num = 0;
-	int received_nums[100] = { 0, };
-	int find_num = 0;
-	int find_stack = 0;
-}ES;
+struct FIND_MAXANDMIN {
+	int num;
+	int max_num;
+	int min_num;
+}fmm;
 
 int main()
 {
 	int i;
+	int temp;
+	cin >> fmm.num;
 
-	cin >> ES.total_num;
-	for (i = 0; i < ES.total_num; i++)
-		cin >> ES.received_nums[i];
-	cin >> ES.find_num;
+	int *rec_nums = (int*)malloc(sizeof(int) * fmm.num);
 
-	for (i = 0; i < ES.total_num; i++) {
-		if (ES.received_nums[i] == ES.find_num)
-			ES.find_stack++;
+	for (i = 0; i < fmm.num; i++)
+		cin >> rec_nums[i];
+
+	fmm.min_num = fmm.max_num = rec_nums[0];
+	for (i = 1; i < fmm.num; i++) {
+		if (fmm.min_num > rec_nums[i]) {
+			temp = fmm.min_num;
+			fmm.min_num = rec_nums[i];
+			rec_nums[i] = temp;
+		}
+
+		if (fmm.max_num < rec_nums[i]) {
+			temp = fmm.max_num;
+			fmm.max_num = rec_nums[i];
+			rec_nums[i] = temp;
+		}
 	}
 
-	cout << ES.find_stack;
+	cout << fmm.min_num << " " << fmm.max_num << endl;
 
+	free(rec_nums);
 }
